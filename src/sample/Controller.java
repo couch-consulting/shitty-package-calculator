09a655e49d0686c33c01d541420ad849fdfa6f3a
Rick <it16213@lehre.dhbw-stuttgart.de>
2017-09-15 14:13:57 +0200

    added unit labels with binding to combobox added close event added calculation stub

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,81 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+    @FXML
+    private TextField width, height, depth, weight, result;
+
+    @FXML
+    private Label wUnit, hUnit, dUnit, weightUnit;
+
+    @FXML
+    private ComboBox lengthUnitBox, weightUnitBox;
+
+    @FXML
+    private MenuItem closeItem;
+
+    private ObjectProperty<ULength> lengthProp = new SimpleObjectProperty<>();
+    private ObjectProperty<UWeight> weightProp = new SimpleObjectProperty<>();
+
+
+    public void initialize() {
+
+        closeItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                Platform.exit();
+            }
+        });
+
+        width.textProperty().addListener((observable, oldValue, newValue) -> {
+            result.setText(width.getText() + height.getText() + depth.getText() + weight.getText());
+        });
+        height.textProperty().addListener((observable, oldValue, newValue) -> {
+            result.setText(width.getText() + height.getText() + depth.getText() + weight.getText());
+        });
+        depth.textProperty().addListener((observable, oldValue, newValue) -> {
+            result.setText(width.getText() + height.getText() + depth.getText() + weight.getText());
+        });
+        weight.textProperty().addListener((observable, oldValue, newValue) -> {
+            result.setText(width.getText() + height.getText() + depth.getText() + weight.getText());
+        });
+
+        //comboboxes
+        for (ULength u : ULength.values())
+            lengthUnitBox.setItems(FXCollections.observableArrayList(ULength.values()));
+
+
+        lengthProp.addListener((observable, oldValue, newValue) -> {
+
+            wUnit.setText(newValue.toString());
+            hUnit.setText(newValue.toString());
+            dUnit.setText(newValue.toString());
+        });
+        lengthProp.bind(lengthUnitBox.valueProperty());
+
+        lengthUnitBox.setValue(ULength.cm);
+
+//
+        for (UWeight u : UWeight.values())
+            weightUnitBox.setItems(FXCollections.observableArrayList(UWeight.values()));
+
+        weightProp.addListener((observable, oldValue, newValue) -> {
+            weightUnit.setText(newValue.toString());
+        });
+        weightProp.bind(weightUnitBox.valueProperty());
+
+        weightUnitBox.setValue(UWeight.g);
+    }
 }
